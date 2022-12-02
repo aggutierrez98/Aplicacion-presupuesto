b73489e553d54c8b0caccf1d54a08fef3baa5d68
Agustin Gutierrez <agustinguti123@gmail.com>
2022-12-01 22:31:21 -0300

    Añadidas mejoras logicas como tambien en estilos y codigo

--- js/app.js
+++ js/app.js
@@ -1,19 +1,15 @@
-let ingresos = [
-    new Ingreso("Salario", 2100),
-    new Ingreso("Venta coche", 1500)
-];
-let egresos = [
-    new Egreso("Renta departamento", 900),
-    new Egreso("Ropa", 400)
-];
+const incomesData = JSON.parse(localStorage.getItem("incomes-data") ?? "[]");
+const outcomesData = JSON.parse(localStorage.getItem("outcomes-data") ?? "[]");
+const incomes = incomesData.map(income => new Ingreso(income._nombre, income._valor))
+const outcomes = outcomesData.map(outcome => new Egreso(outcome._nombre, outcome._valor))
 
 function formatoMoneda(valor) {
     return valor.toLocaleString("es-AR", { style: "currency", currency: "ARS", minimumFractionDigits: 2 })
 }
 
 function mostrarPresupuesto() {
-    let totalEgresos = mostrarEgresos();
     let totalIngresos = mostrarIngresos();
+    let totalEgresos = mostrarEgresos();
     let total = (totalIngresos) - totalEgresos;
 
     document.querySelector(".cabecero-valor").innerHTML = `<h1>${formatoMoneda(total)}</h1>`
@@ -23,41 +19,82 @@ function mostrarPresupuesto() {
 function agregarPorcentajes(totalEgresos, totalIngresos) {
     if (totalIngresos == 0) {
         let porcentajeTotal = 100
-        document.querySelector(".cabecero-egreso-valor").innerHTML += `<div class="porcentaje-cabecera">%${porcentajeTotal.toFixed(2)}</div>`
+        document.querySelector(".cabecero-egreso-valor").innerHTML += `
+        <div class="porcentaje-cabecera">
+            %${porcentajeTotal.toFixed(2)}
+        </div>`
     } else {
         let porcentajeTotal = (totalEgresos / totalIngresos) * 100;
-        document.querySelector(".cabecero-egreso-valor").innerHTML += `<div class="porcentaje-cabecera">%${porcentajeTotal.toFixed(2)}</div>`
+        document.querySelector(".cabecero-egreso-valor").innerHTML += `
+        <div class="porcentaje-cabecera">
+            %${porcentajeTotal.toFixed(2)}
+        </div>`
     }
-
 }
 
 function mostrarIngresos() {
     let totalIngresos = 0;
-    document.querySelector(".abajo-ingreso-valor").innerHTML = "";
-    for (let ingreso of ingresos) {
+    const ingresos = document.querySelector(".abajo-ingreso-valor");
+    ingresos.innerHTML = "";
+
+    for (let ingreso of incomes) {
         totalIngresos += ingreso.valor;
-        document.querySelector(".abajo-ingreso-valor").innerHTML += `<div class="valores-ingreso"><p>${ingreso.nombre}</p><div class="valores-in"> <p>${formatoMoneda(ingreso.valor)}</p><button class="btn" onclick="eliminarIngreso(${ingreso.id})">
-        <ion-icon name="close-circle-outline"></ion-icon>
-    </button></div></div>`
+        ingresos.innerHTML += `
+        <div class="valores-ingreso">
+            <p>${ingreso.nombre}</p>
+            <div class="valores-in"> 
+                <p>${formatoMoneda(ingreso.valor)}</p>
+                <button class="btn" onclick="eliminarIngreso(${ingreso.id})">
+                    <ion-icon name="close-circle-outline"></ion-icon>
+                </button>
+            </div>
+        </div>`
     }
+
     document.querySelector(".cabecero-ingreso-valor").innerHTML = `${formatoMoneda(totalIngresos)}`
+
+    if (incomes.length === 0) {
+        ingresos.classList.add("sin-ingresos")
+        ingresos.setAttribute('data-text', 'No incomes');
+    } else {
+        ingresos.classList.remove("sin-ingresos");
+    }
     return totalIngresos;
 }
 
 function mostrarEgresos() {
     let totalEgresos = 0;
-    document.querySelector(".abajo-egreso-valor").innerHTML = "";
+    const egresos = document.querySelector(".abajo-egreso-valor")
+    egresos.innerHTML = "";
     let porcentaje = 0;
-    for (let egreso of egresos) {
+    for (let egreso of outcomes) {
         totalEgresos += egreso.valor;
     }
-    for (let egreso of egresos) {
+    for (let egreso of outcomes) {
         porcentaje = (egreso.valor / totalEgresos) * 100;
-        document.querySelector(".abajo-egreso-valor").innerHTML += `<div class="valores-egreso"><p>${egreso.nombre}</p><div class="valores-eg"> <p>-${formatoMoneda(egreso.valor)}</p> <div class="porcentaje"> %${porcentaje.toFixed(2)}</div><button class="btn" onclick="eliminarEgreso(${egreso.id})">
-        <ion-icon name="close-circle-outline"></ion-icon>
-    </button></div></div>`
+        egresos.innerHTML += `
+        <div class="valores-egreso">
+            <p>${egreso.nombre}</p>
+            <div class="valores-eg"> 
+                <p>-${formatoMoneda(egreso.valor)}</p> 
+                <div class="porcentaje"> 
+                    %${porcentaje.toFixed(2)}
+                </div>
+                <button class="btn" onclick="eliminarEgreso(${egreso.id})">
+                <ion-icon name="close-circle-outline"></ion-icon>
+                </button>
+            </div>
+        </div>`
     }
     document.querySelector(".cabecero-egreso-valor").innerHTML = `-${formatoMoneda(totalEgresos)}`
+
+    if (outcomes.length === 0) {
+        egresos.classList.add("sin-egresos")
+        egresos.setAttribute('data-text', 'No outcomes');
+    } else {
+        egresos.classList.remove("sin-egresos")
+    }
+
     return totalEgresos;
 }
 
@@ -68,62 +105,32 @@ function agregarDato() {
     let valor = formulario["valor"];
     if (descripcion.value !== "" && valor.value !== "") {
         if (tipo.value === "ingreso") {
-            ingresos.push(new Ingreso(descripcion.value, +valor.value));
+            incomes.push(new Ingreso(descripcion.value, +valor.value));
         } else if (tipo.value === "egreso") {
-            egresos.push(new Egreso(descripcion.value, +valor.value));
+            outcomes.push(new Egreso(descripcion.value, +valor.value));
         }
         mostrarPresupuesto();
+        actualizarLocalStorage();
     }
 }
 
 function eliminarIngreso(id) {
-    let indice = ingresos.findIndex(ingreso => ingreso.id === id);
-    ingresos.splice(indice, 1);
+    let indice = incomes.findIndex(ingreso => ingreso.id === id);
+    incomes.splice(indice, 1);
     mostrarPresupuesto();
+    actualizarLocalStorage()
 }
 
 function eliminarEgreso(id) {
-    let indice = egresos.findIndex(ingreso => ingreso.id === id);
-    egresos.splice(indice, 1);
+    let indice = outcomes.findIndex(ingreso => ingreso.id === id);
+    outcomes.splice(indice, 1);
     mostrarPresupuesto();
+    actualizarLocalStorage()
 }
 
-
-
-const addCart = (e) => {
-    if (e.target.classList.contains("btn")) {
-        const index = searchProduct(e.target.name, productos);
-
-        if (index != null) {
-            const exist = searchProduct(e.target.name, cart);
-
-            if (exist == null) {
-                cart.push(productos[index]);
-                drawMiniChar();
-                msgSuccess();
-            } else {
-                msgError("Ya compraste este producto");
-            }
-        } else {
-            msgError("Error, no se encontro el producto!");
-        }
-    }
-}
-
-function searchProduct(productName, searchArray) {
-    let index = searchArray.length;
-    let i = 0;
-
-    while (index == searchArray.length && i < searchArray.length) {
-        if (searchArray[i].nombre == productName) {
-            index = i;
-        }
-        i++;
-    }
-
-    if (index != searchArray.length) {
-        return index;
-    } else {
-        return null;
-    }
+function actualizarLocalStorage() {
+    const incomesData = JSON.stringify(incomes);
+    const outomcesData = JSON.stringify(outcomes);
+    localStorage.setItem("incomes-data", incomesData)
+    localStorage.setItem("outcomes-data", outomcesData)
 }
